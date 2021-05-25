dfa1adbb7e58007408d2279fd769b852a9a8e265
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-05-26 01:29:23 +0300

    Add project files.

---
+++ MernisValidator/Data/MernisService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Mernis.KPSPublicSoapClient;
+
+namespace MernisValidator.Data
+{
+    public class MernisService
+    {
+        public Task<bool> TcKimlikDogrula(User user)
+        {
+            bool dogrulamaSonucu = false;
+            try
+            {
+                var mernisClient = new Mernis.KPSPublicSoapClient(EndpointConfiguration.KPSPublicSoap);
+                var tcKimlikDogrulamaServisResponse = mernisClient.TCKimlikNoDogrulaAsync(long.Parse(user.TcKimlikNo), user.Ad, user.Soyad, user.DogumTarihi.Year).GetAwaiter().GetResult();
+                dogrulamaSonucu = tcKimlikDogrulamaServisResponse.Body.TCKimlikNoDogrulaResult;
+            }
+            catch (Exception ex)
+            {
+                dogrulamaSonucu = false;
+            }
+            return Task.FromResult(dogrulamaSonucu);
+        }
+    }
+}
---
+++ MernisValidator/Data/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MernisValidator.Data
+{
+    public class User
+    {
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public string TcKimlikNo { get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+    }
+}
